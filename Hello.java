b751b8c13b0d7d703328ea7a60859878b58e6614
Timothy J Hickey <tjhickey@brandeis.edu>
2017-11-20 10:34:01 -0500

    Added Japanese printing method

--- Hello.java
+++ Hello.java
@@ -2,4 +2,8 @@ public class Hello {
   public static void main(String[] args){
     System.out.println("Hello World!!!!!");
   }
+
+  public static void printJapanese(){
+    System.out.println("山");
+  }
 }
